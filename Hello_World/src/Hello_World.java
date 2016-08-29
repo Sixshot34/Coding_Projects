b62fff09b550833377c8dc8ec4e10b4b1be901d8
Sixshot34 <Janoah99@gmail.com>
2016-08-29 08:48:31 -0700

    Hello World Demonstration
    This program was easy. I hope to have more rigorous projects in the
    future.

---
+++ Hello_World/src/Hello_World.java
@@ -0,0 +1,15 @@
+/*
+ * The Hello World routine known as the shortest program can
+ * print out any string into the Console by using a println
+ * method.
+ * 
+ * @author (Janoah_Ablang) 
+ * @version (1.0_8/25/2016)
+ */
+public class Hello_World
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From <Janoah_Ablang>"); //Display the string
+    }
+}
